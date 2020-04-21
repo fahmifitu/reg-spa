c9a7f8648a3610b75baf1cf3d0e2a77ece011825
Fahmi Elfituri <fahmi.fitu@gmail.com>
2020-04-21 19:48:28 +0200

    fix

---
+++ public/dist/js/209888b4706b2d7a0a1c.js
@@ -0,0 +1,14 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[12],{
+
+/***/ "./resources/js/lang/ar.json":
+/*!***********************************!*\
+  !*** ./resources/js/lang/ar.json ***!
+  \***********************************/
+/*! exports provided: ok, cancel, error_alert_title, error_alert_text, token_expired_alert_title, token_expired_alert_text, login, register, page_not_found, go_home, logout, email, remember_me, password, forgot_password, confirm_password, name, toggle_navigation, home, you_are_logged_in, reset_password, send_password_reset_link, settings, profile, your_info, info_updated, update, your_password, password_updated, new_password, login_with, register_with, verify_email, send_verification_link, resend_verification_link, failed_to_verify_email, verify_email_address, default */
+/***/ (function(module) {
+
+eval("module.exports = {\"ok\":\"نعم\",\"cancel\":\"الغاء\",\"error_alert_title\":\"Oops...\",\"error_alert_text\":\"حصل خطأ ما! الرجاء المحاولة مرة اخرى.\",\"token_expired_alert_title\":\"انتهت صلاحية الجلسة!\",\"token_expired_alert_text\":\"الرجاء تسجيل الدخول مرة اخري.\",\"login\":\"دخول\",\"register\":\"تسجيل\",\"page_not_found\":\"الصفحة غير موجودة\",\"go_home\":\"Go Home\",\"logout\":\"خروج\",\"email\":\"البريد الإلكتروني\",\"remember_me\":\"تذكرني\",\"password\":\"كلمة المرور\",\"forgot_password\":\"نسيت كلمة المرور?\",\"confirm_password\":\"تأكيد كلمة المرور\",\"name\":\"الإسم\",\"toggle_navigation\":\"Toggle navigation\",\"home\":\"الرئيسية\",\"you_are_logged_in\":\"تم تسجيل الدخول!\",\"reset_password\":\"Reset Password\",\"send_password_reset_link\":\"Send Password Reset Link\",\"settings\":\"Settings\",\"profile\":\"Profile\",\"your_info\":\"Your Info\",\"info_updated\":\"Your info has been updated!\",\"update\":\"Update\",\"your_password\":\"Your Password\",\"password_updated\":\"Your password has been updated!\",\"new_password\":\"New Password\",\"login_with\":\"Login with\",\"register_with\":\"Register with\",\"verify_email\":\"Verify Email\",\"send_verification_link\":\"Send Verification Link\",\"resend_verification_link\":\"Resend Verification Link ?\",\"failed_to_verify_email\":\"Failed to verify email.\",\"verify_email_address\":\"We sent you an email with an the verification link.\"};//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIuL3Jlc291cmNlcy9qcy9sYW5nL2FyLmpzb24uanMiLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./resources/js/lang/ar.json\n");
+
+/***/ })
+
+}]);
