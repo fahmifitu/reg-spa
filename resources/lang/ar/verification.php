3cb580c7d38dbaf1a1fdf1c5e626396a2a92d1fa
Fahmi Elfituri <fahmi.fitu@gmail.com>
2020-04-21 17:54:21 +0200

    First push

---
+++ resources/lang/ar/verification.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'verified' => 'تم التحقق من بريدك الإلكتروني',
+    'invalid' => 'هناك خطأ في رابط التفعيل.',
+    'already_verified' => 'البريد الإلكتروني مفعل مسبقا.',
+    'user' => 'We can\'t find a user with that e-mail address.',
+    'sent' => 'تم ارسال رابط التفعيل علي بريدك الإلكتروني!',
+];
